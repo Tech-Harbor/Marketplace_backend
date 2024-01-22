429fc568f1b92ea24ca615478f2e1ed9ac2f5763
Vladik <v4054741@gmail.com>
2024-01-22 08:22:37 +0300

    Update config Cloudinary

--- src/main/java/com/example/backend/Config/CloudinaryConfig.java
+++ src/main/java/com/example/backend/Config/CloudinaryConfig.java
@@ -3,23 +3,28 @@ package com.example.backend.Config;
 import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.beans.factory.annotation.Value;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
-    private final String CLOUD_NAME = "dg6onp4zj";
-    private final String API_KEY = "737533466716681";
-    private final String API_SECRET = "RLZwbVf3JEdmOaCO5JHo4G-XEI8";
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary(){
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", CLOUD_NAME);
-        config.put("api_key", API_KEY);
-        config.put("api_secret", API_SECRET);
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 

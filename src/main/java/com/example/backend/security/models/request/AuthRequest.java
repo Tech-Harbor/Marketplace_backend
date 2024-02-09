6c7dd98f475a2137c948c68970be2c0c5403bbe1
Vladik <v4054741@gmail.com>
2024-02-09 20:22:36 +0300

    Update validation auth

--- src/main/java/com/example/backend/security/models/request/AuthRequest.java
+++ src/main/java/com/example/backend/security/models/request/AuthRequest.java
@@ -4,4 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record AuthRequest(@Email @NotNull @NotBlank String email, @NotNull @NotBlank String password) {}
+public record AuthRequest(
+        @NotNull @NotBlank @Email String email,
+        @NotNull @NotBlank String password) {}
--- src/main/java/com/example/backend/security/models/request/RegisterRequest.java
+++ src/main/java/com/example/backend/security/models/request/RegisterRequest.java
@@ -1,11 +1,13 @@
 package com.example.backend.security.models.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
-public record RegisterRequest(@NotNull @NotBlank @Size(min = 3, max = 20) String lastname,
-                              @NotNull @NotBlank @Size(min = 3, max = 20) String firstname,
-                              @NotNull @NotBlank @Size(max = 9) String number, @Email String email,
-                              @NotNull @NotBlank @Size(min = 3, max = 20) String password) {}
+public record RegisterRequest(
+        @NotNull @NotBlank @Size(min = 2, max = 20)
+        @Pattern(regexp = "^[A-Z][a-z]*(\\s(([a-z]{1,3})|(([a-z]+\\')?[A-Z][a-z]*)))*$") String lastname,
+        @NotNull @NotBlank @Size(min = 2, max = 20)
+        @Pattern(regexp = "^[A-Z][a-z]*(\\s(([a-z]{1,3})|(([a-z]+\\')?[A-Z][a-z]*)))*$") String firstname,
+        @NotNull @NotBlank @Size(max = 9) String number,
+        @NotNull @NotBlank @Email @Size(min = 7, max = 35) String email,
+        @NotNull @NotBlank @Size(min = 7, max = 20)
+        @Pattern(regexp = "^[A-Z][a-z]*(\\s(([a-z]{1,3})|(([a-z]+\\')?[A-Z][a-z]*)))*$")String password) {}

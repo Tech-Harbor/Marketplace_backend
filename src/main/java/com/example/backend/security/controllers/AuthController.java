53f4691901dcf31544f97063aa981bd5e19d634d
Vladik <v4054741@gmail.com>
2024-02-11 13:43:57 +0300

    Update Swagger, added security

--- src/main/java/com/example/backend/security/controllers/AuthController.java
+++ src/main/java/com/example/backend/security/controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.example.backend.security.models.request.AuthRequest;
 import com.example.backend.security.models.request.RegisterRequest;
 import com.example.backend.security.models.response.AuthResponse;
 import com.example.backend.security.service.AuthService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Tag(name = "AuthSystem", description = "Точка входу в систему і особистий кабінет, користувача")
 public class AuthController {
 
     private final static String SIGNUP_URI = "/signup";
@@ -22,11 +25,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(SIGNUP_URI)
+    @SecurityRequirement(name = "Bearer Authentication")
     public AuthResponse signup(@RequestBody @Validated RegisterRequest registerRequest) {
         return authService.signup(registerRequest);
     }
 
     @PostMapping(LOGIN_URI)
+    @SecurityRequirement(name = "Bearer Authentication")
     public AuthResponse login(@RequestBody @Validated AuthRequest authRequest) {
         return authService.login(authRequest);
     }

db2f487cf70e0011a460b3c0b44ab864e6d132ea
Vladik <v4054741@gmail.com>
2024-02-24 13:10:02 +0300

    Added logic one level below

--- src/main/java/com/example/backend/web/File/ImageService.java
+++ src/main/java/com/example/backend/web/File/ImageService.java
@@ -1,10 +1,12 @@
 package com.example.backend.web.File;
 
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface ImageService {
-    ImageDTO save(ImageEntity imageEntity);
+    ImageDTO uploadImage(MultipartFile file);
     List<ImageDTO> getAllPhoto();
 
     ImageDTO imageById(Long id);
--- src/main/java/com/example/backend/web/File/ImageServiceImpl.java
+++ src/main/java/com/example/backend/web/File/ImageServiceImpl.java
@@ -1,9 +1,17 @@
 package com.example.backend.web.File;
 
+import com.example.backend.web.File.utils.FileUpload;
+import com.example.backend.web.exception.request.BadRequestException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -11,11 +19,26 @@ import java.util.stream.Collectors;
 public class ImageServiceImpl implements ImageService{
 
     private final ImageRepository imageRepository;
+    private final FileUpload fileUpload;
     private final ImageFactory imageFactory;
 
     @Override
-    public ImageDTO save(ImageEntity imageEntity) {
-        return imageFactory.makeImageFactory(imageRepository.save(imageEntity));
+    @SneakyThrows
+    public ImageDTO uploadImage(MultipartFile file){
+        Optional<BufferedImage> imageOptional = Optional.ofNullable(ImageIO.read(file.getInputStream()));
+
+        imageOptional.orElseThrow(() -> new BadRequestException("There is no uploaded image"));
+
+
+        Map result = fileUpload.uploadFile(file);
+
+        ImageEntity image = new ImageEntity(
+                (String) result.get("original_filename"),
+                (String) result.get("url"),
+                (String) result.get("public_id")
+        );
+
+        return imageFactory.makeImageFactory(imageRepository.save(image));
     }
 
     @Override

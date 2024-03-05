45d8de666e2c626e4c22873a9b0d4754820d08c3
PetsaYuri <petsa.yuri@gmail.com>
2024-03-05 10:36:16 +0200

    Created the ExceptionController.java class to track various exceptions and created the ErrorResponseDTO.java class to customise the data that is sent.

---
+++ src/main/java/com/example/backend/web/exception/ErrorResponseDTO.java
@@ -0,0 +1,6 @@
+package com.example.backend.web.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponseDTO(LocalDateTime timeStamp, String error, int status, String message, String path) {
+}

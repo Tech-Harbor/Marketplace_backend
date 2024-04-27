32da22368d279c3eee1dffe647637657d2504991
Vladik <v4054741@gmail.com>
2024-04-27 16:28:22 +0300

    Added annotations

---
+++ src/main/java/com/example/backend/utils/annotations/ApiResponseBadRequest.java
@@ -0,0 +1,13 @@
+package com.example.backend.utils.annotations;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(responseCode = "400", description = "Some data is missing")
+public @interface ApiResponseBadRequest { }

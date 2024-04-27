5073c71dbb9248d6abc0cb1a77803785f28c3a9e
Vladik <v4054741@gmail.com>
2024-04-27 10:49:57 +0300

    Update link

--- src/main/java/com/example/backend/utils/general/Constants.java
+++ src/main/java/com/example/backend/utils/general/Constants.java
@@ -18,4 +18,5 @@ public class Constants {
     public static final String PASSWORD = "password";
     public static final String JWT = "JWT";
     public static final String TYPE = "type";
+    public static final String ROLE = "role";
 }

45180beeafd1499612148e929dc0faf75a4f5c8a
Vladik <v4054741@gmail.com>
2024-05-03 16:03:23 +0300

    Create annotation for accouth

---
+++ src/main/java/com/example/backend/utils/annotations/ApiResponseInfoOK.java
@@ -0,0 +1,28 @@
+package com.example.backend.utils.annotations;
+
+import com.example.backend.web.User.store.dto.UserInfoDTO;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok", content = {
+                @Content(mediaType = APPLICATION_JSON_VALUE, schema =
+                @Schema(implementation = UserInfoDTO.class))}
+        ),
+    }
+)
+@ResponseStatus(value = HttpStatus.OK)
+public @interface ApiResponseInfoOK { }

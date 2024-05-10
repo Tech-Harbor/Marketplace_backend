bfd8244e74e33d8c6b83bfbdf89857ada5c03c04
Vladik <v4054741@gmail.com>
2024-05-10 21:09:24 +0300

    fix ImageEntity

--- src/main/java/com/example/backend/web/File/ImageEntity.java
+++ src/main/java/com/example/backend/web/File/ImageEntity.java
@@ -12,17 +12,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ImageEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String imageUrl;
     private String imageId;
-
-    public ImageEntity(final String name, final String imageUrl, final String imageId) {
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.imageId = imageId;
-    }
 }

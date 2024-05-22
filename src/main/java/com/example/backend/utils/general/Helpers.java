dda5ecf9e814d72481d13236e03b28a28a5e3e03
Vladik <v4054741@gmail.com>
2024-05-22 23:03:59 +0300

    Added class Helpers

---
+++ src/main/java/com/example/backend/utils/general/Helpers.java
@@ -0,0 +1,30 @@
+package com.example.backend.utils.general;
+
+
+import com.example.backend.security.service.JwtService;
+import com.example.backend.web.User.UserService;
+import com.example.backend.web.User.store.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class Helpers {
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    public UserEntity tokenUserData(final String jwt) {
+        final var token = jwtService.extractUserData(jwt.substring(7));
+
+        return userService.getByUserData(token);
+    }
+
+    public Optional<UserEntity> tokenUserEmail(String jwt) {
+        final var token = jwtService.extractUserData(jwt.substring(7));
+
+        return userService.getByEmail(token);
+    }
+}

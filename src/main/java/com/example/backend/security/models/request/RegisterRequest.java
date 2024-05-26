1bdee5c1cd6a719d00f5122bb3462c8557509685
Vladik <v4054741@gmail.com>
2024-05-26 22:50:29 +0300

    Update models for auth

--- src/main/java/com/example/backend/security/models/request/RegisterRequest.java
+++ src/main/java/com/example/backend/security/models/request/RegisterRequest.java
@@ -7,22 +7,24 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
 
+import static com.example.backend.utils.general.Constants.EMPTY_FIELD;
+
 @Builder
 public record RegisterRequest(
-        @NotNull @NotBlank(message = "Поле не повинно бути порожнім")
-        @Size(min = 2, max = 20, message = "Довжина firstname має бути між 2 та 20 символами")
+        @NotNull @NotBlank(message = EMPTY_FIELD)
+        @Size(min = 2, max = 20, message = "Довжина lastname має бути між 2 та 20 символами")
         @Pattern(regexp = "^[A-Z][a-z]*(\\s(([a-z]{1,3})|(([a-z]+\\')?[A-Z][a-z]*)))*$",
                 message = "lastname має відповідати вказаному формату") String lastname,
-        @NotNull @NotBlank(message = "Поле не повинно бути порожнім")
+        @NotNull @NotBlank(message = EMPTY_FIELD)
         @Size(min = 2, max = 20, message = "Довжина firstname має бути між 2 та 20 символами")
         @Pattern(regexp = "^[A-Z][a-z]*(\\s(([a-z]{1,3})|(([a-z]+\\')?[A-Z][a-z]*)))*$",
                 message = "firstname має відповідати вказаному формату") String firstname,
-        @NotNull @NotBlank(message = "Поле не повинно бути порожнім")
+        @NotNull @NotBlank(message = EMPTY_FIELD)
         @Size(min = 12, max = 12, message = "Номер не валідний, повинен містить 12 символів") String phone,
-        @NotNull @NotBlank(message = "Поле не повинно бути порожнім")
+        @NotNull @NotBlank(message = EMPTY_FIELD)
         @Email(message = "Введіть коректну адресу електронної пошти з @")
         @Size(min = 7, max = 35, message = "Довжина email має бути між 7 та 35 символами") String email,
-        @NotNull @NotBlank(message = "Поле не повинно бути порожнім")
+        @NotNull @NotBlank(message = EMPTY_FIELD)
         @Size(min = 7, max = 20, message = "Довжина password має бути між 7 та 20 символами")
         @Pattern(regexp = "^(?=.*\\d)[A-Za-z\\d]+$",
                 message = "password має відповідати вказаному формату") String password) { }

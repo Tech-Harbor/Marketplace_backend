bcbb3a7c1772e462292485a286c83df9b9f28381
Vladik <v4054741@gmail.com>
2024-05-29 21:03:39 +0300

    Create AdvertisementUpdateDTO and AdvertisementUpdateFactory

---
+++ src/main/java/com/example/backend/web/Advertisement/store/factory/AdvertisementUpdateFactory.java
@@ -0,0 +1,23 @@
+package com.example.backend.web.Advertisement.store.factory;
+
+import com.example.backend.web.Advertisement.store.AdvertisementEntity;
+import com.example.backend.web.Advertisement.store.dto.AdvertisementUpdateDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class AdvertisementUpdateFactory implements Function<AdvertisementEntity, AdvertisementUpdateDTO> {
+    @Override
+    public AdvertisementUpdateDTO apply(final AdvertisementEntity entity) {
+        return AdvertisementUpdateDTO.builder()
+                .descriptionAdvertisement(entity.getDescriptionAdvertisement())
+                .characteristicAdvertisement(entity.getCharacteristicAdvertisement())
+                .name(entity.getName())
+                .price(entity.getPrice())
+                .category(entity.getCategory().getName())
+                .location(entity.getLocation())
+                .delivery(entity.getDelivery())
+                .build();
+    }
+}

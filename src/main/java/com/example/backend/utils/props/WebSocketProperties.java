36cc9ee690ee2ff720c9d60e2ebe8c4021db0536
Vladik <v4054741@gmail.com>
2024-06-24 18:40:32 +0300

    Added WebSocketConfig and application.yaml for webSocket

---
+++ src/main/java/com/example/backend/utils/props/WebSocketProperties.java
@@ -0,0 +1,20 @@
+package com.example.backend.utils.props;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "spring.socket")
+public class WebSocketProperties {
+
+    private String[] destPrefixes;
+    private String appPrefix;
+    private String endpoint;
+    private String topicPrefix;
+    private String notifyPrefix;
+    private String errorPrefix;
+    private int TimeToFirstMessage;
+
+}

d6ef0a376040c75447872fbc49f6766944c8c601
Vladik <v4054741@gmail.com>
2024-06-24 18:40:50 +0300

    Added WebSocketConfig

---
+++ src/main/java/com/example/backend/config/websocket/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.example.backend.config.websocket;
+
+
+import com.example.backend.utils.props.WebSocketProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketProperties webSocketProperties;
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker(webSocketProperties.getDestPrefixes());
+        registry.setApplicationDestinationPrefixes(webSocketProperties.getAppPrefix());
+    }
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint(webSocketProperties.getEndpoint());
+        registry.addEndpoint(webSocketProperties.getEndpoint()).setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureWebSocketTransport(final WebSocketTransportRegistration registry) {
+        registry.setTimeToFirstMessage(webSocketProperties.getTimeToFirstMessage());
+    }
+}

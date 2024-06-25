46b731cc16cee6339bd8f83e2d290705d66dca72
Vladik <v4054741@gmail.com>
2024-06-25 16:51:08 +0300

    Added method configureMessageConverters for WebSocket

--- src/main/java/com/example/backend/websocket/WebSocketConfig.java
+++ src/main/java/com/example/backend/websocket/WebSocketConfig.java
@@ -1,12 +1,20 @@
 package com.example.backend.websocket;
 
 import com.example.backend.utils.props.WebSocketProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @RequiredArgsConstructor
 @Configuration
@@ -26,5 +34,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry.addEndpoint(webSocketProperties.getEndpoint());
         registry.addEndpoint(webSocketProperties.getEndpoint()).setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public boolean configureMessageConverters(final List<MessageConverter> messageConverters) {
+        final var resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(APPLICATION_JSON);
+
+        final var converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+
+        messageConverters.add(converter);
+        return false;
     }
 }

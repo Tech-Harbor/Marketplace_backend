6c1ac31a76a685c73724a0ecdd8087bc008d9d89
Vladik <v4054741@gmail.com>
2024-07-01 13:48:21 +0300

    Added method deleteActiveAdvertisement and updateActiveAdvertisement for Advertisement and update class Test

---
+++ src/main/java/com/example/backend/utils/general/SchedulerProject.java
@@ -0,0 +1,32 @@
+package com.example.backend.utils.general;
+
+import com.example.backend.web.Advertisement.AdvertisementRepository;
+import com.example.backend.web.User.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@AllArgsConstructor
+public class SchedulerProject {
+
+    private final AdvertisementRepository advertisementRepository;
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 3600000)
+    public void deleteUsers() {
+        userRepository.deleteNotValidatedUsers(LocalDateTime.now().minusHours(1));
+    }
+
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void deleteActiveAdvertisement () {
+        advertisementRepository.deleteActiveAdvertisements(LocalDateTime.now());
+    }
+
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void updateActiveAdvertisement () {
+        advertisementRepository.updateActiveAdvertisements(LocalDateTime.now());
+    }
+}
--- src/main/java/com/example/backend/web/Advertisement/AdvertisementRepository.java
+++ src/main/java/com/example/backend/web/Advertisement/AdvertisementRepository.java
@@ -1,10 +1,26 @@
 package com.example.backend.web.Advertisement;
 
 import com.example.backend.web.Advertisement.store.AdvertisementEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface AdvertisementRepository extends JpaRepository<AdvertisementEntity, Long> {
     AdvertisementEntity getByName(String name);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AdvertisementEntity ad SET ad.active = false WHERE ad.createDate <= :dateTime AND ad.active = true")
+    void updateActiveAdvertisements(@Param("dateTime") LocalDateTime dateTime);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM AdvertisementEntity ad WHERE ad.createDate <= :dateTime AND ad.active = false")
+    void deleteActiveAdvertisements(@Param("dateTime") LocalDateTime dateTime);
 }

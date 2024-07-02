228d8196a3176fb5a2b68721a6a68c5ef5a3ec20
Vladik <v4054741@gmail.com>
2024-07-02 14:40:45 +0300

    fix: MyUserDetails

--- src/main/java/com/example/backend/security/service/details/MyUserDetails.java
+++ src/main/java/com/example/backend/security/service/details/MyUserDetails.java
@@ -3,12 +3,9 @@ package com.example.backend.security.service.details;
 import com.example.backend.utils.enums.Role;
 import com.example.backend.web.User.store.dto.UserSecurityDTO;
 import lombok.Builder;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @Builder
 public record MyUserDetails(UserSecurityDTO user) implements UserDetails {

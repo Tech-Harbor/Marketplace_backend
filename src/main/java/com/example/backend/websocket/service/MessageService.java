7ba3d2b5568b44e706e34f2e67e02bf373c3e37e
Vladik <v4054741@gmail.com>
2024-07-08 11:09:43 +0300

    DeleteAll WebSocket chat

--- src/main/java/com/example/backend/websocket/service/MessageService.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.websocket.service;
-
-import com.example.backend.websocket.store.entities.ReceivedMessageEntity;
-import com.example.backend.websocket.store.entities.SendMessageEntity;
-
-public interface MessageService {
-    SendMessageEntity sendToTopic(ReceivedMessageEntity receivedMessage, String username);
-    SendMessageEntity sendToUser(ReceivedMessageEntity receivedMessage, String username);
-}
--- src/main/java/com/example/backend/websocket/store/repository/ReceivedMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.websocket.store.repository;
-
-import com.example.backend.websocket.store.entities.ReceivedMessageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReceivedMessageRepository extends JpaRepository<ReceivedMessageEntity, Long> { }

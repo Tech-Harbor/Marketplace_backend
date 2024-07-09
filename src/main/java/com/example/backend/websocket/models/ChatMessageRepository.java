17e1f06b8d256d4d8faadbb46e91b5fda3139e8a
Vladik <v4054741@gmail.com>
2024-07-09 14:52:10 +0300

    Added ChatMessageEntity and ChatMessageRepository.java

---
+++ src/main/java/com/example/backend/websocket/models/ChatMessageRepository.java
@@ -0,0 +1,11 @@
+package com.example.backend.websocket.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, Long> {
+    List<ChatMessageEntity> findByChatId(String chatId);
+}

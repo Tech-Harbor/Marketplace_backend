dae3328fa795420b74bf4c32ba696511a97671e6
Vladik <v4054741@gmail.com>
2024-07-14 18:25:18 +0300

    Added Operation for AdvertisementController

--- src/main/java/com/example/backend/web/Advertisement/AdvertisementController.java
+++ src/main/java/com/example/backend/web/Advertisement/AdvertisementController.java
@@ -6,6 +6,7 @@ import com.example.backend.utils.annotations.ApiResponseOK;
 import com.example.backend.web.Advertisement.store.dto.AdvertisementCreateDTO;
 import com.example.backend.web.Advertisement.store.dto.AdvertisementDTO;
 import com.example.backend.web.Advertisement.store.dto.AdvertisementUpdateDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -31,6 +32,7 @@ public class AdvertisementController {
     private static final String URL_DELETE_ALL = "/deleteAll";
 
     @PostMapping(value = URL_CREATE, consumes = {MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "Current user create advertisements")
     @ApiResponseCreated
     public AdvertisementCreateDTO createAdvertisementByUser(@RequestHeader(AUTHORIZATION) final String jwt,
                                                             @RequestPart final AdvertisementCreateDTO advertisement,
@@ -44,12 +46,14 @@ public class AdvertisementController {
     }
 
     @GetMapping(ADVERTISEMENT)
+    @Operation(summary = "Current user get advertisement")
     @ApiResponseOK
     public AdvertisementDTO getByAdvertisement(@RequestHeader(AUTHORIZATION) final String jwt) {
         return advertisementServer.advertisement(jwt);
     }
 
     @PatchMapping(URL_EDIT)
+    @Operation(summary = "Current user update advertisement")
     @ApiResponseOK
     public AdvertisementUpdateDTO editAdvertisement(@RequestHeader(AUTHORIZATION) final String jwt,
                                                     @RequestBody final AdvertisementUpdateDTO entity) {
@@ -57,12 +61,14 @@ public class AdvertisementController {
     }
 
     @DeleteMapping(URL_DELETE)
+    @Operation(summary = "Current user delete advertisement")
     @ApiResponseDelete
     public void deleteAdvertisement(@RequestHeader(AUTHORIZATION) final String jwt) {
         advertisementServer.deleteAdvertisement(jwt);
     }
 
     @DeleteMapping(URL_DELETE_ALL)
+    @Operation(summary = "Current user will remove all advertisements")
     @ApiResponseDelete
     public void deleteAllAdvertisement(@RequestHeader(AUTHORIZATION) final String jwt) {
         advertisementServer.deleteAll(jwt);

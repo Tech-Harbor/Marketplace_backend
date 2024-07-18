ca9809830ed72c197a97a969144e624a533cb792
Vladik <v4054741@gmail.com>
2024-07-18 12:49:48 +0300

    Update annotation

--- src/main/java/com/example/backend/websocket/controller/ChatController.java
+++ src/main/java/com/example/backend/websocket/controller/ChatController.java
@@ -5,13 +5,13 @@ import com.example.backend.websocket.servers.ChatMessageServer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ChatController {
 
